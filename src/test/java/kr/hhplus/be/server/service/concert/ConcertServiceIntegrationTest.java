e5c234a4b142262ea3176ae73a74073069f7d07c
kimbro97 <rlagudwog@gmail.com>
2025-05-22 13:15:13 +0900

    test: concertService addranking 단위 통합 테스트 작성

--- src/test/java/kr/hhplus/be/server/service/concert/ConcertServiceIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/service/concert/ConcertServiceIntegrationTest.java
@@ -4,12 +4,16 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Set;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.hhplus.be.server.domain.concert.Concert;
@@ -39,6 +43,21 @@ class ConcertServiceIntegrationTest {
 
 	@Autowired
 	private ScheduleJpaRepository scheduleJpaRepository;
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@BeforeEach
+	void setUp() {
+		Set<String> keys = stringRedisTemplate.keys("concert:*:schedule:*:count");
+		if (keys != null && !keys.isEmpty()) {
+			stringRedisTemplate.delete(keys);
+		}
+		Set<String> sortedSet = stringRedisTemplate.keys("concert:ranking:*");
+		if (sortedSet != null && !sortedSet.isEmpty()) {
+			stringRedisTemplate.delete(sortedSet);
+		}
+	}
 
 	@Test
 	@DisplayName("스케줄 조회시 존재하지 않는 콘서트면 예외가 발생한다")
@@ -118,6 +137,81 @@ class ConcertServiceIntegrationTest {
 
 		// assert
 		assertThat(info.size()).isEqualTo(4);
+	}
+
+	@Test
+	@DisplayName("예약 좌석 수가 스케줄 좌석 수와 다르면 랭킹을 갱신하지 않는다.")
+	void add_ranking_increase_success() {
+	    // arrange
+		Concert concert = new Concert("아이유 10주년 콘서트");
+		concertJpaRepository.save(concert);
+
+		Schedule schedule = new Schedule(concert, LocalDate.of(2025, 4, 1), LocalDateTime.now());
+		scheduleJpaRepository.save(schedule);
+
+		Seat seat1 = new Seat(schedule, "1", 1000L, false);
+		Seat seat2 = new Seat(schedule, "2", 1000L, false);
+
+		seatJpaRepository.saveAll(List.of(seat1, seat2));
+		LocalDateTime today = LocalDateTime.now();
+
+		ConcertCommand.AddRanking command = ConcertCommand.AddRanking
+			.builder()
+			.paymentId(1L)
+			.concertId(concert.getId())
+			.scheduleId(schedule.getId())
+			.openedAt(today)
+			.scheduleDate(LocalDate.now())
+			.today(today)
+			.build();
+		// act
+		concertService.addRanking(command);
+	    // assert
+		String count = stringRedisTemplate.opsForValue()
+			.get("concert:" + concert.getId() + ":schedule:" + schedule.getId() + ":count");
+		assertThat(count).isEqualTo("1");
+
+		String ranking = stringRedisTemplate.opsForValue()
+			.get("concert:ranking:" + today.format(DateTimeFormatter.BASIC_ISO_DATE));
+
+		assertThat(ranking).isNull();
+	}
+
+	@Test
+	@DisplayName("예약 좌석 수가 스케줄 좌석 수와 같으면 랭킹을 갱신한다.")
+	void add_ranking_success() {
+		// arrange
+		Concert concert = new Concert("아이유 10주년 콘서트");
+		concertJpaRepository.save(concert);
+
+		Schedule schedule = new Schedule(concert, LocalDate.of(2025, 4, 1), LocalDateTime.now());
+		scheduleJpaRepository.save(schedule);
+
+		Seat seat1 = new Seat(schedule, "1", 1000L, false);
+
+		seatJpaRepository.saveAll(List.of(seat1));
+		LocalDateTime today = LocalDateTime.now();
+
+		ConcertCommand.AddRanking command = ConcertCommand.AddRanking
+			.builder()
+			.paymentId(1L)
+			.concertId(concert.getId())
+			.scheduleId(schedule.getId())
+			.openedAt(today)
+			.scheduleDate(LocalDate.now())
+			.today(today)
+			.build();
+		// act
+		concertService.addRanking(command);
+		// assert
+		String count = stringRedisTemplate.opsForValue()
+			.get("concert:" + concert.getId() + ":schedule:" + schedule.getId() + ":count");
+		assertThat(count).isEqualTo("1");
+
+		Set<String> range = stringRedisTemplate.opsForZSet()
+			.range("concert:ranking:" + today.format(DateTimeFormatter.BASIC_ISO_DATE), 0, 15);
+
+		assertThat(range).hasSize(1);
 	}
 
 }
